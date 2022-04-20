8396a1513f8d491dcc9f66abb7af4c5d2df95693
charles <charles.obasi@nuenergy.ai>
2022-04-19 22:54:07 -0400

    added twinkling-stars-animation

---
+++ src/backgroundAnimation/Animation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Animation.scss';
+
+const Animation = () => {
+	return (
+		<>
+			<div className="stars"></div>
+			<div className="twinkling"></div>
+		</>
+	);
+};
+
+export default Animation;
