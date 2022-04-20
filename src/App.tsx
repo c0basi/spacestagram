5b9e0b882e91fca3cff9721138cfb609cb7133ba
charles <charles.obasi@nuenergy.ai>
2022-04-19 23:22:49 -0400

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Animation from './backgroundAnimation/Animation';
+import Header from './components/Header/Header';
 function App() {
 	return (
 		<div className="App">
 			{/* <div className="stars"></div>
 			<div className="twinkling"></div> */}
 			<Animation />
+			<Header />
 			<div>Make some food</div>
 		</div>
 	);
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './Header.scss';
 
 const Header = () => {
-	return <div></div>;
+	return (
+		<div className="header">
+			<h1 className="header--title">Spacestagram</h1>
+			<span className="header--credits">Nasa Api</span>
+		</div>
+	);
 };
 
 export default Header;
